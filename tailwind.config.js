7bbcd25c2f029cad8b99ac051b5573efeaf5ef1d
ridwancodexr <ridwancodexr@gmail.com>
2025-11-20 07:10:38 +0600

    all update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["*"],
   theme: {
-    fontSize:{
-      'ridwan': ['5rem']
-    } ,
-    fontFamily:{
-      'display': ['oswald']
-    } ,
+    
     extend: {},
   },
   plugins: [],
