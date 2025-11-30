8edbaa3699550b71fb9532a4ed35c7d2c1ddcad5
ridwancodexr <ridwancodexr@gmail.com>
2025-11-30 13:47:29 +0600

    js file create

---
+++ about.js
@@ -0,0 +1,8 @@
+ function sideBar () {
+   const bar =  document.querySelector("#bar")
+   bar.style.display = 'none'
+ }
+ function openbar () {
+   const bar =  document.querySelector("#closBar")
+   bar.style.display = 'flex'
+ }
